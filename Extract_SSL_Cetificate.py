908ffc61a156345d02ace4d61b505be0a243ee7f
sachinkayan <sachin.mtk@gmail.com>
2020-02-14 18:09:37 +0530

    Add files via upload

---
+++ Extract_SSL_Cetificate.py
@@ -0,0 +1,85 @@
+import os.path
+from os import path
+import os.path
+import commands
+from datetime import date
+
+hostname = commands.getoutput('hostname -a')
+hostname1 = commands.getoutput('hostname')
+certificate = hostname1+'.pfx'
+environment = 'test'
+cert_dir1 = '/tardis2/ssl_certificates/'+environment
+cert_dir2 = '/tardis2/sachin/certificates/PROD/certificates/'+environment
+
+
+print "Certificate name is "+certificate
+#print "Hostname is "+hostname
+print "Hostname is "+hostname1
+print "----------------------------------------------"
+
+working_dir = commands.getoutput('pwd')
+
+#print "Current directory is "+working_dir
+
+commands.getoutput('cp -p /tardis2/ssl_certificates/test/'+hostname1+'.pfx '+'./certificates/PROD/')
+
+res = os.path.isfile('/tardis2/sachin/certificates/PROD/'+certificate)
+
+print "Copy status "+ str(res)
+print "----------------------------------------------"
+
+if res == True:
+
+        print "Certificate is avaialbe in source directory"
+
+        print "Provide credentials for extracting CERT"
+        print "->"
+        a=commands.getoutput\
+        ('openssl pkcs12 -in /tardis2/sachin/certificates/'
+                   'PROD/'+certificate+ ' -clcerts -nokeys -out /tardis2/sachin/'
+                                        'certificates/PROD/ca-'+hostname1+'-03-15-2022.cert')
+        print "Cert extraction result : "+a
+
+        if "MAC verified OK" in a:
+                print "Provide credentials for extracting KEY"
+                print "->"
+                b=commands.getoutput\
+                ('openssl pkcs12 -in /tardis2/sachin/certificates/'
+                   'PROD/'+certificate+ ' -nocerts -out /tardis2/sachin/'
+                                        'certificates/PROD/ca-'+hostname1+'-03-15-2022.key')
+                print "Key extraction result : "+b
+        else:
+                commands.getoutput('rm /tardis2/sachin/certificates/PROD/ca-'+hostname1+'-03-15-2022.cert')
+                print "Extraction of CERT failed"
+                exit()
+
+        if "MAC verified OK" in b:
+                print "Provide credentials for extracting KEY"
+                print "->"
+                c=commands.getoutput\
+                ('openssl pkcs12 -in /tardis2/sachin/certificates/'
+                   'PROD/'+certificate+ ' -out /tardis2/sachin/'
+                                        'certificates/PROD/ca-'+hostname1+'-03-15-2022.crt -nodes -nokeys -cacerts')
+
+                print "CRT extraction result : "+c
+
+                if "MAC verified OK" in c:
+                        print "--------------------------------------------------"
+                        print "CERT, KEY and CRT files are generated successfully"
+                        print "--------------------------------------------------"
+                else:
+                        commands.getoutput('rm /tardis2/sachin/certificates/PROD/ca-'+hostname1+'-03-15-2022.key')
+                        commands.getoutput('rm /tardis2/sachin/certificates/PROD/ca-'+hostname1+'-03-15-2022.cert')
+                        commands.getoutput('rm /tardis2/sachin/certificates/PROD/ca-'+hostname1+'-03-15-2022.crt')
+                        print "Please run the script again with correct credentials"
+                        exit()
+        else:
+                commands.getoutput('rm /tardis2/sachin/certificates/PROD/ca-'+hostname1+'-03-15-2022.key')
+                commands.getoutput('rm /tardis2/sachin/certificates/PROD/ca-'+hostname1+'-03-15-2022.cert')
+                print "Extraction of KEY failed"
+                exit()
+
+else:
+        print "Certificate is not available in the source  directory"
+        exit()
+
---
+++ LPSD_Query_Count.py
@@ -0,0 +1,68 @@
+import smtplib
+import string
+import sqlite3
+from sqlite3 import Error
+import commands
+from datetime import datetime
+
+str1 = ""
+
+currentMonth = int(datetime.now().month)
+currentYear = int(datetime.now().year)
+
+
+if currentMonth == 1:
+    lastMonth = 12
+else:
+    lastMonth = currentMonth-1
+
+
+smtpServer = 'intramail.cis.cat.com'  # SMTP server that handles email requests
+host = commands.getoutput('hostname')
+fromaddr = 'Latest query report' + '@' + host
+toaddr=['S_sachin@cat.com']
+subj = 'Latest query report for '+str(currentYear)+'-'+str(lastMonth)
+
+fpath = '/tardis2/sachin/sqldb_report_log' #A file located in tardis mount to log the report generation
+lfile = open(fpath, mode='r')
+
+reportDate = str(lastMonth)+str(currentYear)
+
+for line in lfile:                          # Check whether the report is already generated for the server.
+        if reportDate+"-"+host in line:
+                print "Report already generated for this server"
+                exit()
+else:
+        print "Generating report...."
+
+try:
+        if lastMonth in (10,11,12):
+                lastMonth = lastMonth
+        else:
+                lastMonth = str(0)+str(lastMonth)
+
+        path='/vivisimo/velocity/data/reporting/'+str(currentYear)+'-'+str(lastMonth)
+        print "Path is "+str(path)
+        conn = sqlite3.connect(path)
+        #print conn
+        curs = conn.cursor()
+        curs.execute("SELECT count(*) FROM reporting;")
+        for data1 in curs.fetchall():
+                a="Latest Query count for "+str(currentYear)+ " Month "+str(lastMonth)+" is :" +str(data1)+" --  "
+
+        lfile = open(fpath, mode='a')
+        lfile.write(reportDate+"-"+host+ "\n")
+        print a
+        body=a
+
+        def email(toaddr, fromaddr, subj, body, smtpServer):
+                msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n" % (fromaddr, string.join(toaddr, ", "), subj))
+                server = smtplib.SMTP(smtpServer)
+                server.sendmail(fromaddr, toaddr, msg + body)
+                server.quit()
+
+        email(toaddr, fromaddr, subj, body, smtpServer)
+
+except Error:
+        print "Database file is not available "+str(Error)
+        exit()
